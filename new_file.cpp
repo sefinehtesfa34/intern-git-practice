eb1ab7c4fc46d71c2a2ff835f117eef76ac4a8d1
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2023-03-24 09:50:25 +0300

    new commit to this repository

---
+++ new_file.cpp
@@ -0,0 +1,29 @@
+#include<algorithm>
+#include<iostream>
+#include<vector>
+#include<cmath>
+using namespace std;
+
+void dfs(int root, int parent){
+    
+}
+int main(){
+    int n;
+    cin>>n;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
